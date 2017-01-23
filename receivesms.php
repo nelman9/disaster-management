c0c6a97d58e4a5b435e3772edcefbb32f2cd6293
nelman9 <nelsonombongi90@gmail.com>
2017-01-23 14:05:55 -0600

    Add files via upload

--- receivesms.php
+++ receivesms.php
@@ -8,32 +8,12 @@ define("DBPASSWORD","nelman",true);
 
 define("DBNAME","disaster",true);
 
-function checkusrReg($sender)
-	{
-		$con = 'mysql:dbname='.DBNAME.';host='.DBHOST;
-$cmd = new PDO($con,DBUSERNAME,DBPASSWORD);
-       $SQL = "SELECT m_no,f_name FROM reg_details WHERE m_no=$sender";
-        $STH = $cmd->prepare($SQL);
-                   $STH->execute();
-                      $row = $STH->fetch();
-                    if ($STH->rowCount()> 0) {
-
-                    	insertSms($_GET['sender'],$_GET['text'],$row['f_name']);
-                    }else{
-
-                    	notreg($_GET['sender'],$_GET['text']);
-
-                    	echo 'THANK YOU FOR THE INFORMATION, NAKURU REDCROSS WILL CALL YOU FOR MORE DETAILS AND CONFIRMATION';
-                    }
-                      
-
-	}
 
 
-function insertSms($sender,$text,$name)
+function insertSms($sender,$text)
 
 {	
-	
+
 	$con = 'mysql:dbname='.DBNAME.';host='.DBHOST;
 
 	try {
@@ -56,10 +36,8 @@ function insertSms($sender,$text,$name)
 
 				{
 
-					echo "Hello ".$name.". THANK YOU FOR YOUR INFORMATION NAKURU RED-CROSS WILL CALL YOU FOR CONFIRMATION.";
-					fowardsms($text);
-
-                                       // header('location:forward.php?text='.$text.'');
+					echo "Hello ".$text.". Thank you for your registration.";
+                                        header('location:forward.php');
 
 				}
 
@@ -67,7 +45,7 @@ function insertSms($sender,$text,$name)
 
 				{
 
-					echo "Sorry an error has occured nakuru redcross is working on it";
+					echo "Sorry an error has occured";
 
 				}
 
@@ -84,86 +62,9 @@ function insertSms($sender,$text,$name)
 }
 
 
-function fowardsms($text)
-{
-
-
-	$con = 'mysql:dbname='.DBNAME.';host='.DBHOST;
-
-	try {
-
-    		$cmd = new PDO($con,DBUSERNAME,DBPASSWORD);
-
-		
-
-		$SQL = "SELECT m_no FROM staffs";
-            $STH = $cmd->prepare($SQL);
-                   $STH->execute();
-                      $row = $STH->fetch();
-                    if ($STH->rowCount()> 0) {
-                      
-                      while ($row = $STH->fetch()) {
-                         $m_no = $row['m_no'];
-                         $STH = $cmd->prepare("INSERT INTO send_sms(receiver,msgdata) VALUES ('$m_no','$text')");
-
-			$STH->bindParam(':m_no',$sender);
-
-			$STH->bindParam(':text',$text);
-
-			$STH->execute();
-
-			$cmd = null;
-                         //echo $m_no;
-    }
-    
-
-                             }	
-			
-			}		 
-
-		catch (PDOException $e) {
-
-    		echo 'Connection failed: ' . $e->getMessage();
-
-			}
 
 
 
-}
-
-
-function notreg($m_no,$text){
-	$text=$text.'From ' .$m_no;
-
-$con = 'mysql:dbname='.DBNAME.';host='.DBHOST;
-$cmd = new PDO($con,DBUSERNAME,DBPASSWORD);
-       $SQL = "SELECT m_no FROM reg_details WHERE type='admin'";
-        $STH = $cmd->prepare($SQL);
-                   $STH->execute();
-                     
-                          $row = $STH->fetch();
-                    if ($STH->rowCount()> 0) {
-                      
-                      while ($row = $STH->fetch()) {
-                         $m_no = $row['m_no'];
-                         $STH = $cmd->prepare("INSERT INTO send_sms(receiver,msgdata) VALUES ('$m_no','$text')");
-
-			$STH->bindParam(':m_no',$sender);
-
-			$STH->bindParam(':text',$text);
-
-			$STH->execute();
-
-			$cmd = null;
-                         //echo $m_no;
-    }
-    
-
-                             }	
-
-
-}
-checkusrReg($_GET['sender']);
+insertSms($_GET['sender'],$_GET['text']);
 
 ?>
-
